8b870a8dde18d7443aaf1fdd565a1e9b28293f80
Steve Kerr <sker078@aucklanduni.ac.nz>
2025-05-31 20:53:18 +1200

    use state for recommendations

--- src/components/Recommendations/Recommendations.tsx
+++ src/components/Recommendations/Recommendations.tsx
@@ -2,13 +2,9 @@ import type { FC } from "react";
 import { Contents, PaperContent, Wrapper } from "./style";
 import { Grid, Paper, Typography } from "@mui/material";
 
-const recommendations = [
-  "Steven consistently demonstrates a rare ability to move seamlessly between frontend and backend systems. His work on optimizing API performance and building scalable infrastructure has had a direct impact on the stability and speed of our platforms. He’s the kind of engineer who not only solves problems but improves the system around them.",
-  "Steven is a standout full stack developer not only for his technical skills but also for his ability to collaborate cross-functionally. He communicates complex ideas clearly and brings empathy and thoughtfulness to technical discussions, making him a valuable bridge between engineering, product, and design teams.",
-  "Dependable, sharp, and always a step ahead—Steven is the kind of engineer you want on every project. He takes ownership, writes clean and maintainable code, and consistently delivers on time. His initiative and leadership mindset make him an asset to any high-performing team.",
-];
+type Recommendation = string;
 
-const Recommendation: FC<{ comment: string }> = ({ comment }) => (
+const Recommendation: FC<{ comment: Recommendation }> = ({ comment }) => (
   <Paper elevation={0} variant="outlined">
     <PaperContent>
       <Typography variant="body2">
@@ -18,7 +14,9 @@ const Recommendation: FC<{ comment: string }> = ({ comment }) => (
   </Paper>
 );
 
-export const Recommendations: FC = () => (
+export const Recommendations: FC<{ recommendations: Recommendation[] }> = ({
+  recommendations,
+}) => (
   <Wrapper>
     <Typography variant="h5" sx={{ color: "rgb(124 58 237)", fontWeight: 800 }}>
       Recommendations
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,4 +1,4 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import { Page } from "./styles";
 import { Bar } from "../../components/AppBar/AppBar";
 import { AboutMe } from "../../components/AboutMe/AboutMe";
@@ -6,14 +6,21 @@ import { Skills } from "../../components/Skills/Skills";
 import { Projects } from "../../components/Projects/Projects";
 import { Recommendations } from "../../components/Recommendations/Recommendations";
 
+const mockRecommendations = [
+  "Steven consistently demonstrates a rare ability to move seamlessly between frontend and backend systems. His work on optimizing API performance and building scalable infrastructure has had a direct impact on the stability and speed of our platforms. He’s the kind of engineer who not only solves problems but improves the system around them.",
+  "Steven is a standout full stack developer not only for his technical skills but also for his ability to collaborate cross-functionally. He communicates complex ideas clearly and brings empathy and thoughtfulness to technical discussions, making him a valuable bridge between engineering, product, and design teams.",
+  "Dependable, sharp, and always a step ahead—Steven is the kind of engineer you want on every project. He takes ownership, writes clean and maintainable code, and consistently delivers on time. His initiative and leadership mindset make him an asset to any high-performing team.",
+];
+
 export const Main: FC = () => {
+  const [recommendations, setRecommendations] = useState(mockRecommendations);
   return (
     <Page>
       <Bar />
       <AboutMe />
       <Skills />
       <Projects />
-      <Recommendations />
+      <Recommendations recommendations={recommendations} />
     </Page>
   );
 };
